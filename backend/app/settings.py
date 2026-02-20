cc5ab77f97c44703e1b82938b863d5f58f53f2d7
Gabriel Santos <gab.tec97@gmail.com>
2026-02-20 19:51:24 -0300

    feat: creates a simple healthcheck endpoint with test

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -28,7 +28,7 @@ DEBUG = config("DEBUG", default=False, cast=bool)
 ALLOWED_HOSTS = config(
     "ALLOWED_HOSTS",
     default="",
-    cast=lambda v: [s.strip() for s in v.split(",")]
+    cast=lambda v: [s.strip() for s in v.split(",")],
 )
 
 
--- backend/app/urls.py
+++ backend/app/urls.py
@@ -18,9 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from api.urls import urlpatterns as api_urls
-
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include(api_urls)),
+    path("api/", include("api.urls")),
 ]
