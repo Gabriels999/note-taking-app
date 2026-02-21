e45d2adb873396385ee032b6b6ab69098b352361
Gabriel Santos <gab.tec97@gmail.com>
2026-02-21 14:05:07 -0300

    feat: creates login and signup endpoints

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,4 +4,7 @@ from api import views
 
 urlpatterns = [
     path("health/", views.health_check, name="health-check"),
+    path("auth/csrf/", views.csrf_cookie, name="csrf-cookie"),
+    path("auth/login/", views.APILoginView.as_view(), name="auth-login"),
+    path("auth/signup/", views.signup, name="auth-signup"),
 ]
