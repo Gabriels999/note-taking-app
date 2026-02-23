3e589e3e0766fa4883da90fcedac8f9995f2e727
Gabriel Santos <gab.tec97@gmail.com>
2026-02-23 11:06:53 -0300

    feat: vitest config

---
+++ frontend/vitest.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  esbuild: {
+    jsx: "automatic",
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./vitest.setup.ts"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+      include: ["app/**/*.tsx", "components/**/*.tsx", "services/**/*.ts"],
+    },
+  },
+});
